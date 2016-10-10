571bfa9f75f185a1740ec025d61a2a25a3b22fc5
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-10-10 10:21:59 +1000

    Get to running

---
+++ core/src/main/java/org/techern/rsbot/RSBot.java
@@ -0,0 +1,20 @@
+package org.techern.rsbot;
+
+/**
+ * RSBot; The entry class
+ *
+ * @since 0.0.1
+ */
+public class RSBot {
+
+    /**
+     * The entry point
+     * @param arguments Any command-line arguments passed on
+     *
+     * @since 0.0.1
+     */
+    public static void main(String... arguments) {
+        System.out.println("Fuck logging until I'm off from work");
+    }
+
+}
