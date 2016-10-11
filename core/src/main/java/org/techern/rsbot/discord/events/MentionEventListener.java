22b789439ad40acf794ecd813d763d15dfd69857
Cruz Julian Bishop <cruzjbishop@gmail.com>
2016-10-12 08:44:31 +1000

    Temporary shutting up of the bot

--- core/src/main/java/org/techern/rsbot/discord/events/MentionEventListener.java
+++ core/src/main/java/org/techern/rsbot/discord/events/MentionEventListener.java
@@ -3,6 +3,7 @@ package org.techern.rsbot.discord.events;
 import org.techern.rsbot.RSBot;
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.MentionEvent;
+import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RateLimitException;
@@ -39,8 +40,17 @@ public class MentionEventListener implements IListener<MentionEvent> {
                 RSBot.LOGGER.error("Error while resetting chat bot", e);
             }
             RSBot.BOT_SESSIONS.remove(event.getMessage().getAuthor());
+        } else if (messageText.toLowerCase().contains("fucking shut up")) {
+            try {
+                for (IMessage messages : event.getMessage().getChannel().getMessages()) {
+                    if (messages.getAuthor().equals(event.getClient().getOurUser())) {
+                        messages.delete();
+                    }
+                }
+            } catch (MissingPermissionsException | DiscordException | RateLimitException e) {
+                RSBot.LOGGER.error("Error while pruning chat bot", e);
+            }
         } else {
-
             try {
                 event.getMessage().getChannel().sendMessage(RSBot.getChatBotSession(event.getMessage()).think(messageText));
             } catch (MissingPermissionsException | DiscordException | RateLimitException e) {
